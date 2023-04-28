1fcca8a9ee66d889d439a248f0df6be1a543e71c
JakubKub13 <jakubkubala3@gmail.com>
2023-04-28 11:57:36 +0200

    sequential_chain implementation

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from apiKey import apiKey
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SimpleSequentialChain
+from langchain.chains import LLMChain, SequentialChain
 
 os.environ['OPENAI_API_KEY'] = apiKey
 
@@ -29,14 +29,15 @@ script_template = PromptTemplate(
 # CREATE INSTANCE OF OUR LLM SERVICE
 llm = OpenAI(temperature=0.9)
 # chain multiple llms together by langchain
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
-script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True)
-sequential_chain = SimpleSequentialChain(chains=[title_chain, script_chain], verbose=True)
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title')
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script')
+sequential_chain = SequentialChain(chains=[title_chain, script_chain], input_variables=['topic'],  output_variables=['title', 'script'], verbose=True)
 
 # we need to join chains together so they can communicate
 
 # print stuff to the screen if there is a prompt
 if prompt:
-    response = sequential_chain.run(prompt)
-    st.write(response)
+    response = sequential_chain({'topic': prompt})
+    st.write(response['title'])
+    st.write(response['script'])
 
